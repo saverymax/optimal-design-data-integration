6207fa484ce39196f336ac7570a06b8f48f470dd
vsc45956 vscuser <vsc45956@gligar08.gastly.os>
2023-06-06 12:31:35 +0200

    Add new experiments

--- experiment_scripts/make_oe_exps.sh
+++ experiment_scripts/make_oe_exps.sh
@@ -5,7 +5,9 @@ alpha="-2"
 beta="2"
 gamma="-2 -1.5 -1 -0.5 0"
 delta="0.5"
-detection_p="0.2 0.7"
+#detection_p="0.2 0.7"
+detection_p="0.2"
+aux_cor="0.2 0.8"
 # Might be nice to write job output to the experimental run dir but it's nice to leave that dir created by the R script
 # so as to seperate the HPC and local run capabilities.
 WORKDIR=$VSC_DATA/projects/optimal_design_presence_only/optimal-design-data-integration
@@ -17,7 +19,10 @@ for g in $gamma
 do
 for p in $detection_p
 do
-exp_name=optimal_design_model_${m}_gamma_${g}_p_${p}
+for corr in $aux_cor
+do
+#exp_name=optimal_design_model_${m}_gamma_${g}_p_${p}
+exp_name=oe_model-${m}_m-5_r-96_intns-${intensity}_a-${alpha}_b-${beta}_g-${g}_d-${delta}_p-${p}_aux-cor-${corr}
 echo "#!/bin/bash
 #PBS -o /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
 #PBS -e /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
@@ -27,11 +32,9 @@ echo "#!/bin/bash
 #PBS -l mem=100gb
 
 module load CmdStanR
-Rscript $WORKDIR/optimal_design_site_occ.R --working_dir=$WORKDIR --data_reps=$(($cores*2)) --m=5 --model_selection=$m --random_starts=3 --exch_iter=20 --mcmc_iter=1000 --intensity_fun=\"$intensity\" --v_parallel --cores=$cores --alpha=$alpha --beta=$beta --gamma=$g --delta=$delta --p=$p" > $exp_name.sh
+Rscript $WORKDIR/optimal_design_site_occ.R --working_dir=$WORKDIR --exp_name=$exp_name --data_reps=$(($cores*2)) --m=5 --model_selection=$m --random_starts=3 --exch_iter=20 --mcmc_iter=1000 --intensity_fun=\"$intensity\" --v_parallel --cores=$cores --alpha=$alpha --beta=$beta --gamma=$g --delta=$delta --p=$p" --aux_cor=$corr > $exp_name.sh
 echo "qsub $exp_name.sh" >> run_initial_model_comparison.sh
-#echo "qsub optimal_design_model_${m}_gamma_${g}.sh" >> run_initial_model_comparison.sh
-#echo "qsub optimal_design_model_${m}_alpha_${a}.sh" >> run_initial_model_comparison.sh
-
+done
 done
 done
 done
---
+++ experiment_scripts/oe_model-1_m-5_r-96_intns-donut_-2_b-2_g-0_d-0.5_p-0.2_aux-cor-0.8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -o /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
+#PBS -e /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
+#PBS -N oe_model-1_m-5_r-96_intns-donut_-2_b-2_g-0_d-0.5_p-0.2_aux-cor-0.8
+#PBS -l walltime=01:00:00
+#PBS -l nodes=1:ppn=48
+#PBS -l mem=100gb
+
+module load CmdStanR
+Rscript /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/optimal_design_site_occ.R --working_dir=/data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration --exp_name=oe_model-1_m-5_r-96_intns-donut_-2_b-2_g-0_d-0.5_p-0.2_aux-cor-0.8 --data_reps=96 --m=5 --model_selection=1 --random_starts=3 --exch_iter=20 --mcmc_iter=1000 --intensity_fun="donut" --v_parallel --cores=48 --alpha=-2 --beta=2 --gamma=0 --delta=0.5 --p=0.2 --aux_cor=0.8
