2475ff0c41f595ca5c940c2cd2630bb7a69e488a
vsc45956 vscuser <vsc45956@gligar08.gastly.os>
2023-12-04 16:06:33 +0100

    Fix checking when not vary visits

--- optimal_design_site_occ.R
+++ optimal_design_site_occ.R
@@ -444,12 +444,8 @@ for (r_start in 1:random_starts){
           r_survey_data$Y[, visit_idx] <- r_survey_data_n1$Y[, visit_idx]
           stopifnot(all(r_survey_data$Y[, visit_idx]<=exp_args$min_visits))
           # Check that we're selecting right sites
-          if (visit==exp_args$min_visits){
+          if (exp_args$vary_visits==F & visit==exp_args$min_visits){
             stopifnot(nn%in%visit_idx)
-          }
-          # Check that vector is empty if not varying visits
-          if(exp_args$vary_visits==F){
-            stopifnot(length(visit_idx)==0)
           }
           # Don't really need theta as it's only for data generation purposes
           r_survey_data$theta[, visit_idx] <- r_survey_data_n1$theta[, visit_idx]
