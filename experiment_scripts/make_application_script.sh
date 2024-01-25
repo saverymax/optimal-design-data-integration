868a24e2c8347663a34cb19511f23d3d3510f6eb
saverymax <saverymax@gmail.com>
2024-01-25 14:50:23 +0100

    Update applied runs

--- experiment_scripts/make_application_script.sh
+++ experiment_scripts/make_application_script.sh
@@ -1,9 +1,12 @@
+# No vary visits
 exp_dir=application_1
 rm -rf $exp_dir
 mkdir $exp_dir
 rm run_applied_model_comparison.sh
 models="1 2"
+m=10
 detection_p="0.2"
+po_sample_prop="0.01 0.05 0.1 0.2 0.5 1"
 n_surveys="5"
 WORKDIR=$VSC_DATA/projects/optimal_design_presence_only/optimal-design-data-integration
 cores=48
@@ -16,16 +19,19 @@ save_dir=data/ebird
 ebird_dir=ebd_US_bnhnut_201901_201912_smp_relJul-2023
 for model in $models
 do
-exp_name=oe_application_model-${model}_m-5_n-${n_surveys}_r-$(($cores*2))_p-${detection_p}
+for po_p in $po_sample_prop
+do
+exp_name=oe_application_model-${model}_m-5_n-${n_surveys}_r-$(($cores*2))_p-${detection_p}_po_prop-${po_p}
 echo "#!/bin/bash
 #PBS -o /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
 #PBS -e /data/gent/459/vsc45956/projects/optimal_design_presence_only/optimal-design-data-integration/job_output/
-#PBS -N $exp_name
-#PBS -l walltime=48:00:00
+#PBS -N ${exp_dir}_${exp_name}
+#PBS -l walltime=24:00:00
 #PBS -l nodes=1:ppn=$cores
 #PBS -l mem=100gb
 
 module load CmdStanR
-Rscript $WORKDIR/optimal_design_application.R --working_dir=$WORKDIR --save_dir=experimental_runs/$exp_dir --base_data_dir=$BASE_DATA_DIR --data_save_dir=$save_dir --ebird_data_dir=$ebird_dir --map_file=$map_file --landcover_file=$landcover_file --modis_file=$modis_file --elevation_file=$elev_file --exp_name=$exp_name --data_reps=$(($cores*2)) --m=20 --min_visits=1 --max_visits=$n_surveys --vary_visits --random_starts=10 --model_selection=$model --p=$detection_p --exch_iter=40 --mcmc_iter=1000 --v_parallel --cores=$cores" > $exp_dir/$exp_name.sh
+Rscript $WORKDIR/optimal_design_application.R --working_dir=$WORKDIR --save_dir=experimental_runs/$exp_dir --base_data_dir=$BASE_DATA_DIR --data_save_dir=$save_dir --ebird_data_dir=$ebird_dir --map_file=$map_file --landcover_file=$landcover_file --modis_file=$modis_file --elevation_file=$elev_file --exp_name=$exp_name --data_reps=$(($cores*2)) --m=$m --min_visits=1 --max_visits=$n_surveys --random_starts=10 --model_selection=$model --p=$detection_p --po_sample_prop=$po_p --exch_iter=40 --mcmc_iter=1000 --v_parallel --cores=$cores" > $exp_dir/$exp_name.sh
 echo "qsub $exp_dir/$exp_name.sh" >> run_applied_model_comparison.sh
+done
 done
