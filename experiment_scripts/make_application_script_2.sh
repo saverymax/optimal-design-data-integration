f550276013f3c09549fe11428307b04a1d70934e
saverymax <saverymax@gmail.com>
2024-03-21 09:56:41 +0100

    general exp script update

--- experiment_scripts/make_application_script_2.sh
+++ experiment_scripts/make_application_script_2.sh
@@ -5,9 +5,10 @@ mkdir $exp_dir
 rm run_applied_model_comparison_2.sh
 run_time=20
 models="2"
-sites="5 10 15 20"
+sites="5 10"
+#sites="5 10 15 20"
 detection_p="0.2"
-n_surveys="1"
+n_surveys="2 4"
 WORKDIR=$VSC_DATA/projects/optimal_design_presence_only/optimal-design-data-integration
 cores=48
 po_p=1
